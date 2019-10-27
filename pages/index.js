f7717e5b504eb234579f60cb45a6f232a8ba03c2
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-10-27 22:12:51 +0530

    Added example to get redux state and dispatch action from getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Counter from "../src/components/Counter";
+import { increment } from "../src/actions/counterActions";
 
 function index() {
   return (
@@ -9,7 +10,12 @@ function index() {
   );
 }
 
-index.getInitialProps = () => {
+index.getInitialProps = ({ reduxStore }) => {
+  reduxStore.dispatch(increment()); // action will dispatched on page load
+
+  const state = reduxStore.getState(); // returns redux store
+  console.log(state);
+
   return {};
 };
 
